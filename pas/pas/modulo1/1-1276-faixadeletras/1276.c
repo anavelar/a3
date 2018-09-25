922f27b481de1352464b6eacc95a9b1090d8a29b
Ana <cc.anacabral@gmail.com>
2018-09-25 10:54:17 -0300

    pa 1276 - leitura cada caso

--- pas/pas/modulo1/1-1276-faixadeletras/1276.c
+++ pas/pas/modulo1/1-1276-faixadeletras/1276.c
@@ -5,10 +5,11 @@ int main() {
     //1-Leitura de cada caso de teste
     char stringLida[52];
 
-    scanf("%s\n", stringLida);
-
-    //teste
-    printf("A string lida foi %s\n", stringLida);
+    while (scanf("%s\n", stringLida) > 0 )
+    {
+      //teste
+      printf("A string lida foi %s\n", stringLida);
+    }
 
     return 0;
 }
