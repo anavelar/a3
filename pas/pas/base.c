f47fb8294f241b3ed225bb0ba5a6f0b669ea61b6
Ana <cc.anacabral@gmail.com>
2018-09-25 10:34:12 -0300

    Coloquei base do uri no arquivo base

--- pas/pas/base.c
+++ pas/pas/base.c
@@ -1,8 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
- 
+
 int main() {
- 
-    
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
     return 0;
 }
