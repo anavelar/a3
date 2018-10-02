1f9b8096830d10b616cab3ffb34185cbc6ea6940
Ana <cc.anacabral@gmail.com>
2018-10-02 01:38:34 -0300

    Bib Estrutura de Dados Grafos:Matriz Adjacencia - Inicializada

--- auxiliares_bibliotecas/estruturasDeDados/grafoMatrizAdjacencia/matrizAdj.h
+++ auxiliares_bibliotecas/estruturasDeDados/grafoMatrizAdjacencia/matrizAdj.h
@@ -1,4 +1,36 @@
-#ifndef MATRIZADJ
-#define MATRIZADJ
+#include <stdlib.h>
+#include <stdio.h>
+
+//ANTIGO
+//#ifndef MATRIZADJ
+//#define MATRIZADJ
 //assinaturas das funcoes
-#endif
+//#endif
+
+//--------------------------------- ESTRUTURAS DE DADOS
+//-- LISTAS ENCADEADAS
+typedef struct tipoCelulah* apontador;
+typedef struct{
+  int chave;
+} tipoNo;
+typedef struct tipoCelulah{
+  tipoNo no;
+  apontador prox;
+} tipoCelula;
+typedef struct {
+  apontador inicio;
+  apontador fim;
+} tipoLista;
+//-- GRAFOS
+
+
+// ---------------------------------ASSINATURAS DAS FUNCOES
+//-- LISTAS ENCADEADAS
+void ImprimeNo(tipoNo no);
+void InicializaNoSentinela(tipoNo* eNo);
+void CriaListaVazia(tipoLista* eLista);
+int EstaVazia(tipoLista lista);
+void Insere(tipoLista* eLista, tipoNo noNovo);
+//void Remove(tipoLista* eLista);
+void ImprimeLista (tipoLista lista);
+//-- GRAFOS
