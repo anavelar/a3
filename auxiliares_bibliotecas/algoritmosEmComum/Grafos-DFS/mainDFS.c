431fa084cab44a18a6fabf753f8397e6beb832cf
Ana <cc.anacabral@gmail.com>
2018-10-02 11:28:57 -0300

    Bib DFS - Debuguei - Funcionando Monta Grafo lido ok

--- auxiliares_bibliotecas/algoritmosEmComum/Grafos-DFS/mainDFS.c
+++ auxiliares_bibliotecas/algoritmosEmComum/Grafos-DFS/mainDFS.c
@@ -7,12 +7,6 @@
 // ------------------------------------------------------------------
 
 // POSSIVEIS PAUS
-// 1) Funcao InicializaGrafoSemArestas acessando cada item do vetor
-// com +i*tipoLista ----> Eh isso mesmo? e se a alocacao mudar ein??
-// voltar depois para como tava antes
-// 2) Pode ter pau na funcao de inserir arestas no grafo.
-// Passou em todos os testes que fiz mas tô com muito sono e
-// chequei medio, posso checar mais.
 
 // OBS.:
 // 1) Melhorias possiveis:
@@ -56,7 +50,7 @@ int main(){
 
   //Cria as variáveis principais do main
   tipoGrafo Grafo = NULL;
-  int vertices = 6;
+  int vertices = 7;
 
   //Monta a Estrutura de Dados para representar o Grafo lido
   //Cria o Grafo vazio (com o numero de vertices lido)
@@ -65,7 +59,6 @@ int main(){
   //InsereAresta(tipoGrafo* eGrafo, int verticeOrigem, int verticeDestino)
   //Exemplo para uma aresta 1->2
   //InsereAresta(&Grafo, 1, 2);
-  /*
   InsereAresta(&Grafo, 1, 2);
   InsereAresta(&Grafo, 1, 4);
   InsereAresta(&Grafo, 4, 2);
@@ -73,7 +66,15 @@ int main(){
   InsereAresta(&Grafo, 3, 4);
   InsereAresta(&Grafo, 5, 3);
   InsereAresta(&Grafo, 5, 6);
-  */
+
+  //teste
+  int a;
+  for(a=0; a<vertices; a++)
+  {
+    printf("Lista do vertice %d:\n", a);
+    ImprimeLista(Grafo[a]);
+    printf("\n");
+  }
 
   //Faz a varredura do Grafo com o DFS
 
@@ -98,6 +99,7 @@ void CriaListaVazia(tipoLista* eLista)
   eLista->inicio->prox = NULL;
   eLista->fim = eLista->inicio;
   InicializaNoSentinela(&(eLista->inicio->no));
+  //printf("Lista vazia criada.\n");
 }
 
 int EstaVazia(tipoLista lista)
@@ -108,7 +110,7 @@ int EstaVazia(tipoLista lista)
 void Insere(tipoLista* eLista, tipoNo noNovo)
 {
   eLista->fim->prox = (apontador) malloc(sizeof(tipoCelula));
-  eLista->fim = eLista->fim->prox;
+  eLista->fim = eLista->fim->prox; //**************AQUI TVZ MAS NIVIO TB EH ASSIM
   eLista->fim->prox = NULL;
   eLista->fim->no = noNovo;
 }
@@ -144,25 +146,24 @@ void InicializaGrafoSemArestas(tipoGrafo* eGrafo, int numVertices)
 {
   int i;
 
-  *(eGrafo) = (tipoGrafo) malloc(numVertices*sizeof(tipoLista));
+  (*(eGrafo)) = (tipoLista*) malloc(numVertices*sizeof(tipoLista));
 
+  //Inicializa cada uma dessas listas
   for(i=0; i<numVertices; i++)
   {
-    CriaListaVazia((*(eGrafo))+(i*(sizeof(tipoLista))));
+    CriaListaVazia(&((*(eGrafo))[i]));
   }
+
 }
 
 //Insere aresta direcionada: verticeOrigem -> verticeDestino
 void InsereAresta(tipoGrafo* eGrafo, int verticeOrigem, int verticeDestino)
 {
-  // Cria no que vai ser inserido na nova celula na lista de adj de um vertice
+  // Cria Noh que vai ser inserido na nova celula na lista de adj de um vertice
   tipoNo vizinho;
   vizinho.chave = verticeDestino;
 
   //Insere esse vizinho no vertice de Origem
-  Insere(((*(eGrafo))+((verticeOrigem-1)*(sizeof(tipoLista)))), vizinho);
+  Insere(&((*(eGrafo))[verticeOrigem]), vizinho);
 
-  //teste
-  //printf("Nos vizinhos ao no %d: ", verticeOrigem);
-  //ImprimeLista(*((*(eGrafo))+((verticeOrigem-1)*(sizeof(tipoLista)))));
 }
