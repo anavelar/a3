323aff5104f63cd4c554f29ce52039afbeda965d
Ana <cc.anacabral@gmail.com>
2018-10-13 16:54:32 -0300

    TP1 - Organizacao da pasta

--- auxiliares_bibliotecas/exemplo_biblioteca/bibliotecaX.c
+++
@@ -1,6 +0,0 @@
-#include <bibliotecaX.h>
-// Para incluir as estruturas de dados de la
-// e as assinaturas das funcoes
-
-// Aqui no bibliotecaX.c :
-// colocar as funcoes implementadas
--- auxiliares_bibliotecas/exemplo_biblioteca/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "bibliotecaX.h"
-
-int main(){
-  
-  return 0;
-}
