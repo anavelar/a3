d48b984229eb07f33bbfa13b1a7db4bcbd48f470
Ana <cc.anacabral@gmail.com>
2018-10-21 10:43:25 -0300

    pas 2 - 1608 - funciona para os toys mas nao conferi o debug

--- pas/pas/modulo2/1608/testesFuncionamento/1608.c
+++ pas/pas/modulo2/1608/testesFuncionamento/1608.c
@@ -31,19 +31,35 @@ int main() {
   //PROGRAMA MESMO
   //--------------
   scanf("%d\n", &numCasosTeste);
+  //teste
+  printf("Casos de teste: %d.\n", numCasosTeste);
+  //fimteste
+
 
   //Para cada caso de teste
   for(i=0; i<numCasosTeste; i++)
   {
     scanf("%li %d %d\n", &dinheiro, &numIngredientesExistem, &numBolosExistem);
+    //teste
+    printf("Dinheiro:%li, numIngredientesExistes:%d e numBolosExistem:%d.\n", dinheiro, numIngredientesExistem, numBolosExistem);
+    //fimteste
     precoIngrediente = (int*) malloc(numIngredientesExistem*(sizeof(int)));
     for(j=0; j<(numIngredientesExistem-1); j++)
     {
       scanf("%d ", &(precoIngrediente[j]));
     }
-
+    //teste
+    printf("Ingredientes:\n");
+    for(j=0; j<(numIngredientesExistem-1); j++)
+    {
+      printf("Ingrediente %d: preco %d.\n", j, precoIngrediente[j]);
+    }
+    //fimteste
     //leitura do ultimo
     scanf("%d\n", &(precoIngrediente[(numIngredientesExistem-1)]));
+    //teste
+    printf("Ingrediente %d: preco %d.\n", j, precoIngrediente[j]);
+    //fimteste
 
     precoBoloMaisBarato =  100000001;
     //Para cada bolo existente
@@ -52,6 +68,9 @@ int main() {
       //Inicializacao basica
       precoDesseBolo = 0;
       scanf("%d ", &numIngredientesDesteBolo);
+      //teste
+      printf("Ingredientes no %do bolo: %d\n", (j+1), numIngredientesDesteBolo);
+      //fimteste
 
       //Le cada dupla ingrediente e quantidade dele
       for(k=0; k<(numIngredientesDesteBolo-1); k++)
@@ -59,24 +78,41 @@ int main() {
         scanf("%d %d ", &qualIngrediente, &quantidade);
         custoIngrediente = precoIngrediente[qualIngrediente]*quantidade;
         precoDesseBolo = precoDesseBolo + custoIngrediente;
+        //teste
+        printf("Ingrediente %d:%d unidades.", qualIngrediente, quantidade);
+        printf("Logo custa %li colocar o ingrediente %d no bolo.\n", custoIngrediente, qualIngrediente);
+        printf("Apos soma-lo, preco total do bolo vira: %li.\n\n",precoDesseBolo);
+        //fimteste
       }
 
       //leitura do ultimo par:
       scanf("%d %d\n", &qualIngrediente, &quantidade);
       custoIngrediente = precoIngrediente[qualIngrediente]*quantidade;
       precoDesseBolo = precoDesseBolo + custoIngrediente;
+      //teste
+      printf("Ingrediente %d:%d unidades.", qualIngrediente, quantidade);
+      printf("Logo custa %li colocar o ingrediente %d no bolo.\n", custoIngrediente, qualIngrediente);
+      printf("Apos soma-lo, preco total do bolo vira: %li.\n\n",precoDesseBolo);
+      //fimteste
 
       if(precoDesseBolo < precoBoloMaisBarato )
       {
         precoBoloMaisBarato = precoDesseBolo;
       }
+      //teste
+      printf("Bolo mais barato ate agora: %li.\n", precoBoloMaisBarato);
+      //fimteste
 
     }
 
     //Resultado do caso de teste
     maxBolosPossivel = dinheiro / precoBoloMaisBarato; //deve retornar int
+    //teste
+    printf("Dinheiro:%li.Preco Bolo mais barato:%li. Max bolos possivel:%li.\n\n",
+    dinheiro, precoBoloMaisBarato, maxBolosPossivel);
+    //fimteste
 
-    printf("%li\n", maxBolosPossivel);
+    //VOLTAR DEPOIS DO DEBUGADO: printf("%li\n", maxBolosPossivel);
 
     //Reinicia as variaveis para o novo caso de teste
     free(precoIngrediente); //ou algo similiar
---
+++ pas/pas/modulo2/1608/testesFuncionamento/testeDivisao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  long int dinheiro = 60;
+  long int outro = 24;
+  long int resultado = dinheiro / outro;
+
+  printf("O numero, que deve ser inteiro, eh %li.\n", resultado);
+
+  return 0;
+}
