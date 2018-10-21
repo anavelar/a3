08e8433b8474f2fe232e185a8e1183897843e18b
Ana <cc.anacabral@gmail.com>
2018-10-20 23:03:49 -0300

    pas 2 - 1608 - inicio e primeiros testes

---
+++ pas/pas/modulo2/1608/1608.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INICIALIZACAO 0
+
+int main() {
+
+  int numCasosTeste = INICIALIZACAO; //Ate 100
+  long int dinheiro = INICIALIZACAO; //Ate 10^9, 1 bilhao
+  int numIngredientesExistem = INICIALIZACAO;
+  int numBolosExistem = INICIALIZACAO;
+
+  return 0;
+}
---
+++ pas/pas/modulo2/1608/testesFuncionamento/testeLongInt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  long int dinheiro = 1000000000;
+
+  printf("A variavel eh %li.\n", dinheiro);
+
+  return 0;
+}
