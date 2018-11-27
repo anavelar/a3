30755c6aaaaf999cc616839739fb69be133bce71
Ana Avelar <analuiza.cavelar@gmail.com>
2018-11-27 18:33:36 -0200

    pas 3 - 1912 - versao de entrega

--- pas/pas/modulo3/1912/versao2/1912.c
+++ pas/pas/modulo3/1912/versao2/1912.c
@@ -39,8 +39,7 @@ int main(int argc, char *argv[])
         printf(":D\n");
       else
       {
-        Ordena(0, (N-1), tira); //************************************Conferir
-        //double corteMediano(double hBase, double hTeto, double* tira, long int A, long int N)
+        Ordena(0, (N-1), tira);
         resultado = corteMediano(((double) 0.0000), tira[N-1], tira, A, N);
         printf("%.4lf\n", resultado);
       }
@@ -102,34 +101,35 @@ double corteMediano(double hBase, double hTeto, double* tira, long int A, long i
   double metade, area, delta;
   long int i;
 
-  while(hBase <= hTeto)
+  metade = ((hBase+hTeto) / ((double) 2.0000));
+
+  //Area do corte
+  area = (double) 0.0000;
+  //O Vetor esta ordenado em ordem crescente
+  for(i=(N-1); i>=0; i--)
   {
-    metade = ((hBase+hTeto) / ((double) 2.0000));
-
-    //Area do corte
-    area = (double) 0.0000;
-    //O Vetor esta ordenado em ordem crescente
-    for(i=(N-1); i>=0; i--)
-    {
-      if(metade >= tira[i])
-       break;
-      else
-       area = area + (tira[i] - metade);
-    }
-
-    //Condicao de parada
-    if(A > area)
-      delta = A - area;
-    else //(A < area ou sao iguais)
-      delta = area - A;
-
-    if( delta < ((double) 0.0001) )
-      return metade;
-
-    //Nao encontrou
-    if(area < A)
-      hTeto = metade;
-    else //(area > A)
-      hBase = metade;
+    if(metade >= tira[i])
+     break;
+    else
+     area = area + (tira[i] - metade);
   }
+
+  //Condicao de parada
+  if(A > area)
+    delta = A - area;
+  else //(A < area ou sao iguais)
+    delta = area - A;
+
+  if( delta < ((double) 0.0001) )
+    return metade;
+
+  //Nao encontrou
+  if(hBase <= hTeto)
+  {
+    if(area < A)
+      return (corteMediano(hBase, metade, tira, A, N));
+    else //(area > A)
+      return (corteMediano(metade, hTeto, tira, A, N));
+  }
+
 }
