c80f27e29d5e8bea8a3c949d090d5a60fe6884e7
Ana Avelar <analuiza.cavelar@gmail.com>
2018-12-06 21:52:36 -0200

    tp3 - Makefile e estrutura de arquivos pronta

---
+++ tps/tp3/funcoes.h
@@ -0,0 +1,11 @@
+#ifndef FUNCOESTP3
+#define FUNCOESTP3
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//Estruturas de dados
+
+//Assinaturas das funcoes
+
+#endif
--- tps/tp3/tp3.c
+++ tps/tp3/tp3.c
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "funcoes.h"
 
-int main(int argc, char** argv){
+//int main(int argc, char** argv){
+int main(){
 
+  //Declaração de variáveis
+  //FILE* pArqEntrada = NULL;
+  //FILE* pArqSaida = NULL;
   
 
-
+  //lembrar de desalocar
 
   return 0;
 }
